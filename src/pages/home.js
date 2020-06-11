78fd96400afcac3656ee7d908aa2ca2bcc4e10ec
Leovaldez42 <gaurav4.037@gmail.com>
2020-06-11 11:52:53 +0530

    added screams to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Grid from '@material-ui/core/Grid'
 
+import Scream from '../components/Scream'
 export class home extends Component {
     state = {
         screams: null
@@ -18,7 +19,7 @@ export class home extends Component {
     }
     render() {
         let recentScreamsMarkup = this.state.screams ? (
-        this.state.screams.map(scream => <p>{scream.body}</p>)
+        this.state.screams.map((scream) => <Scream scream={scream}/>)
         ) : <p>Loading ...</p>
         return (
             <Grid container spacing={16}>
