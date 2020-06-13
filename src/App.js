2b6276915cca95db0f972b16fff2bde84b8b41e6
Leovaldez42 <gaurav4.037@gmail.com>
2020-06-13 18:49:49 +0530

    added signup

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 // Pages
 import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 // Componenets
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
+const theme = createMuiTheme(themeFile);
+
+// Once login prevent it
+
+let authenticated
+
+const token = localStorage.FBIdToken;
+if(token) {
+    const decodedToken = jwtDecode(token)
+    console.log(decodedToken);
+    if(decodedToken.exp * 1000 < Date.now()){
+      window.location.href = '/login'
+      authenticated = false
+    } else {
+      authenticated = true
     }
-  },
-  typography: {
-    useNextVariants: true
-  }
-})
+}
 
 
 function App() {
@@ -40,8 +41,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home}/>
-              <Route exact path="/login" component={login}/>
-              <Route exact path="/signup" component={signup}/>
+              <AuthRoute exact path="/login" component={login}  authenticated={authenticated}/>
+              <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
             </Switch>
           </div>
         </Router>
