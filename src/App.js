1309c9a766e8edeb7c03251c96bab8925568ac5e
Leovaldez42 <gaurav4.037@gmail.com>
2020-06-16 13:39:30 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import themeFile from './util/theme'
 import jwtDecode from 'jwt-decode'
+// Redux
+import { Provider } from 'react-redux'
+import store from './redux/store'
 // Pages
 import home from './pages/home'
 import login from './pages/login'
@@ -35,18 +38,18 @@ if(token) {
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home}/>
-              <AuthRoute exact path="/login" component={login}  authenticated={authenticated}/>
-              <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <Provider store={store}>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home}/>
+                <AuthRoute exact path="/login" component={login}  authenticated={authenticated}/>
+                <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
+              </Switch>
+            </div>
+          </Router>
+      </Provider>
     </MuiThemeProvider>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -12,43 +12,35 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
+// Redux stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userAction'
+
 const styles = theme => ({
     ...theme.spreadThis
   });    
+
 class login extends Component {
     constructor() {
         super();
         this.state = {
             email: '',
             password: '',
-            loading: false,
             errors: {}
+        }
+    }
+    componentWillReceiveProps(nextProps){
+        if(nextProps.UI.errors){
+            this.setState({ errors: nextProps.UI.errors });
         }
     }
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({
-            loading: true
-        });
         const userData = {
             email: this.state.email,
             password: this.state.password
-        }
-        axios.post('/login', userData)
-            .then(res => {
-                console.log(res.data);
-                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                })
-            })
+        };
+        this.props.loginUser(userData, this.props.history)
     }
 
     handleChange = (event) => {
@@ -58,8 +50,8 @@ class login extends Component {
     }
 
     render() {
-        const { classes } = this.props;
-        const { errors, loading } = this.state
+        const { classes, UI: {loading} } = this.props;
+        const { errors } = this.state
         return (
             <div>
                 <Grid container className={classes.form}>
@@ -86,7 +78,19 @@ class login extends Component {
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    loginUser: PropTypes.func.isRequired,
+    user: PropTypes.func.isRequired,
+    UI: PropTypes.func.isRequired
 }
 
-export default withStyles(styles)(login)
+const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+});
+
+const mapActionToProps = {
+    loginUser
+}
+
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(login))
