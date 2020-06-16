b0fa82c479a98304054b82180d10eb0cb143c2ef
Leovaldez42 <gaurav4.037@gmail.com>
2020-06-16 14:14:44 +0530

    added redux for signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import Logo from '../images/vjti.jpeg'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 // MUI STUFF
@@ -11,6 +10,10 @@ import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
+
+// Redux stuff
+import { connect } from 'react-redux';
+import { signupUser } from '../redux/actions/userAction'
 
 const styles = theme => ({
     ...theme.spreadThis
@@ -25,10 +28,16 @@ class signup extends Component {
             password: '',
             confirmPassword: '',
             handle: '',
-            loading: false,
             errors: {}
         }
     }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.UI.errors){
+            this.setState({ errors: nextProps.UI.errors });
+        }
+    }
+
     handleSubmit = (event) => {
         event.preventDefault();
         this.setState({
@@ -40,22 +49,8 @@ class signup extends Component {
             confirmPassword: this.state.confirmPassword,
             handle: this.state.handle
         }
-        axios.post('/signup', newUserData)
-            .then(res => {
-                console.log(res.data);
-                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                })
-            })
-    }
+        this.props.signupUser(newUserData, this.props.history);
+    };
 
     handleChange = (event) => {
         this.setState({
@@ -64,8 +59,8 @@ class signup extends Component {
     }
 
     render() {
-        const { classes } = this.props;
-        const { errors, loading } = this.state
+        const { classes, UI: { loading } } = this.props;
+        const { errors } = this.state
         return (
             <div>
                 <Grid container className={classes.form}>
@@ -94,7 +89,15 @@ class signup extends Component {
 }
 
 signup.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired,
+    signupUser: PropTypes.func.isRequired
 }
+const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+})
 
-export default withStyles(styles)(signup)
+
+export default connect(mapStateToProps, {signupUser })(withStyles(styles)(signup))
--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 import { Route, Redirect} from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 const AuthRoute = ({component: Component, authenticated, ...rest }) => (
     <Route {...rest} render={(props) => authenticated === true ? <Redirect to='/' /> : <Component {...props} />}/>
 )
-export default AuthRoute
+
+const mapStateToProps = (state) => ({
+    authenticated: state.user.authenticated
+})
+
+AuthRoute.prototype = {
+    user: PropTypes.object.isRequired
+}
+export default connect(mapStateToProps)(AuthRoute)
