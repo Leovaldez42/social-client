11d5febe5d9b9031709fe6e22b53e56a576b9cc1
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-07-15 23:35:24 +0530

    user page created

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { logoutUser, getUserData } from './redux/actions/userAction'
 import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
+import user from './pages/user';
 // Componenets
 import Navbar from './components/layout/Navbar';
 import AuthRoute from './util/AuthRoute';
@@ -47,6 +48,7 @@ function App() {
                 <Route exact path="/" component={home}/>
                 <AuthRoute exact path="/login" component={login} />
                 <AuthRoute exact path="/signup" component={signup}/>
+                <Route exact path="/users/:handle" component={user} />
               </Switch>
             </div>
           </Router>
--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -3,6 +3,7 @@ import { Route, Redirect} from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
+
 const AuthRoute = ({component: Component, authenticated, ...rest }) => (
     <Route {...rest} render={(props) => authenticated === true ? <Redirect to='/' /> : <Component {...props} />}/>
 )
